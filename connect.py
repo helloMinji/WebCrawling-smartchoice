136096f71fb190c3abd9b9d0ddce52541c28fafa
Minji Hong <41939828+helloMinji@users.noreply.github.com>
2019-02-11 10:09:07 +0900

    Delete connect.py

--- connect.py
+++
@@ -1,7 +0,0 @@
-﻿#coding: utf-8
-
-from selenium import webdriver
-
-driver = webdriver.Chrome('chromedriver.exe가 있는 주소')
-
-driver.get('http://www.smartchoice.or.kr/smc/smartreport/evaluatePage.do')
