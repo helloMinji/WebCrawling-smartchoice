500803de4b792c704b81bfa82ea08c04cc82a348
Minji Hong <41939828+helloMinji@users.noreply.github.com>
2019-02-11 10:10:34 +0900

    Delete year.py

--- year.py
+++
@@ -1,9 +0,0 @@
-﻿#coding: utf-8
-
-### 년도 선택
-
-from connect import driver
-
-def setYear(y):
-    year = driver.find_element_by_xpath("//option[@value='" + y + "']") # 파라미터에 접근
-    year.click() # 클릭 -> 박스의 내용이 바뀐다
