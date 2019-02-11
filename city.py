6529343e850cda3318e7b155b8fe0749cb74fa3e
Minji Hong <41939828+helloMinji@users.noreply.github.com>
2019-02-11 10:09:00 +0900

    Delete city.py

--- city.py
+++
@@ -1,9 +0,0 @@
-﻿#coding: utf-8
-
-### 광역시/도 선택
-
-from connect import driver
-
-def setCity(v):
-    city = driver.find_element_by_xpath("//option[@value='" + v + "']") # 파라미터에 접근
-    city.click() # 클릭 -> 박스의 내용이 바뀐다
